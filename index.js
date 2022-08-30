b755c83cb6c1f8d0678e5b0a6f839d467e335264
Kartynnikk <smurfetta7@mail.ru>
2022-08-30 19:55:48 +0300

    edited all comments 30.08.2022

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ if (number > 0) {
 let sum = (a + b < 4) ? "Мало" : "Много";
 
 let sum = (login === 'Сотрудник') ? 'Привет' :
-    (login == 'Директор') ? 'Здравствуйте' :
-    (login == '') ? 'Нет логина' : '';
+    (login === 'Директор') ? 'Здравствуйте' :
+    (login === '') ? 'Нет логина' : '';
 
 
-let number = prompt(" Чему равна переменная а? ", 0);
+let number = +prompt(" Чему равна переменная а? ", 0);
 if (number === 10) {
     alert("True");
 } else {
@@ -32,14 +32,14 @@ if (number === 10) {
 
 
 
-let min = promt(" Введите число", 15);
-if (min >= 0 && min <= 15) {
+let min = +promt(" Введите число", 15);
+if (min >= 0 && min < 15) {
     alert(" Первая часть ");
-} else if (min >= 15 && min <= 30) {
+} else if (min > 15 && min < 30) {
     alert(" Вторая часть ");
-} else if (min > 30 && min <= 45) {
+} else if (min > 30 && min < 45) {
     alert(" Третья часть ");
-} else if (min >= 45 && min <= 60) {
+} else if (min > 45 && min < 60) {
     alert(" Четевертая часть ");
 }
 
@@ -51,3 +51,27 @@ let result = (!a) ? " Верно " : " Неверено ";
 
 let a = promt(" ");
 let result = (a > 0) ? " Верно " : " Неверено ";
+
+
+
+// let name = "John",
+//     admin = name;
+// alert(admin);
+
+// let currentUserName = "Jogh",
+//     ourSpaceName = "Earth";
+// console.log(currentUserName, ourSpaceName);
+
+// let userName = "John";
+// console.log(typeof userName);
+// alert(`hello ${userName}`);
+
+// let userAge = prompt(" Сколько тебе лет?", 25);
+// alert(userAge);
+
+// let message = confirm(" Are you my boss ?");
+// alert(message);
+
+// let a = prompt("Первое число?", 1),
+//     b = prompt("Второе число?", 2);
+// alert(+a + +b);
