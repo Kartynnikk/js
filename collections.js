f52586e52fc01d7b34cd8ec30d25a1bbd2b20227
Kartynnikk <smurfetta7@mail.ru>
2022-10-02 18:43:37 +0300

    edited all comments 02/10/2022

--- collections.js
+++ collections.js
@@ -1,29 +1,26 @@
 // Допустим, у нас есть массив arr.
 // Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr.
 
-function unique(values) {
-
-    let set = new Set(values);
-
-
+const getUniqueValue = (values) => [
+    const set = new Set(values);
     return Array.from(set);
-}
+]
 
-console.log(unique(["Hare", "Krishna", "Hare", "Krishna",
+console.log(getUniqueValue(["Hare", "Krishna", "Hare", "Krishna",
     "Krishna", "Krishna", "Hare", "Hare", ":-O"
 ]));
 
 // Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
 
 
-let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+const arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 
 function aclean(arr) {
 
-    let map = new Map();
+    const map = new Map();
 
     for (let word of arr) {
-        let result = word.toLowerCase().split("").sort().join("");
+        const result = word.toLowerCase().split("").sort().join("");
         map.set(result, word);
     }
     return Array.from(map.values());
@@ -33,10 +30,10 @@ console.log(aclean(arr));
 
 // Мы хотели бы получить массив ключей map.keys() в переменную и далее работать с ними, например, применить метод .push.
 
-let map = new Map();
+const map = new Map();
 map.set("name", "John");
 
-let keys = Array.from(map.keys());
+const keys = Array.from(map.keys());
 keys.push("more");
 
 console.log(keys);
@@ -45,42 +42,38 @@ console.log(keys);
 // Напишите функцию sumSalaries(salaries), которая возвращает сумму всех зарплат с помощью метода Object.values и цикла for..of.
 // Если объект salaries пуст, то результат должен быть 0.
 
-let salaries = {
+const salaries = {
     "John": 100,
     "Pete": 300,
     "Mary": 250
 };
 
-function sumSalaries(salaries) {
-    return Object.values(salaries).reduce((sum, count) => sum + count);
-}
+const sumSalaries = (salaries) => Object.values(salaries).reduce((sum, count) => sum + count);
 
 console.log(sumSalaries(salaries));
 
 // Напишите функцию count(obj), которая возвращает количество свойств объекта:
 
-let user = {
+const user = {
     name: 'John',
     age: 30
 };
 
-function count(user) {
-    return Object.keys(user).length;
-}
+const getCount = (user) => Object.keys(user).length;
 
-console.log(count(user));
+console.log(getCount(user));
 
 // Напишите деструктурирующее присваивание, которое:
 // свойство name присвоит в переменную name.
 // свойство years присвоит в переменную age.
 // свойство isAdmin присвоит в переменную isAdmin (false, если нет такого свойства)
 
-let user = {
+const user = {
     name: "John",
     years: 30,
 };
 
-let { name, years: age, isAdmin = false } = user;
+const { name, years: age, isAdmin = false } = user;
 
 console.log(name);
 console.log(age);
@@ -91,16 +84,15 @@ console.log(isAdmin);
 // Если несколько высокооплачиваемых сотрудников, можно вернуть любого из них.
 // P.S. Используйте Object.entries и деструктурирование, чтобы перебрать пары ключ/значение.
 
-let salaries = {
+const salaries = {
     "John": 100,
     "Pete": 300,
     "Mary": 250,
 };
 
 function topSalary(salaries) {
-
-    let maxSalary = 0;
-    let salaryName = null;
+    const maxSalary = 0;
+    const salaryName = null;
 
     for (let [name, salary] of Object.entries(salaries)) {
         if (maxSalary < salary) {
@@ -116,16 +108,13 @@ console.log(topSalary(salaries));
 // перечитать что за метод сорт и что мы в него ПЕРЕДАЕМ, 
 // написать 2 функции для сортировки - от большего к меньшему и наоборот
 
-let num = [1, 3, 5, 8, 9, 12];
-
+const num = [1, 3, 5, 8, 9, 12];
 num.sort((a, b) => a - b);
 
 console.log(num);
 
 
-
-let num = [1, 3, 5, 8, 9, 12];
-
+const num = [1, 3, 5, 8, 9, 12];
 num.sort((a, b) => b - a);
 
 console.log(num);
